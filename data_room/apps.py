95e757f43fa68843725b72079df894d5a4ccc3cf
Wajn <wajnaq1@gmail.com>
2026-05-13 11:39:02 +0300

    Initialize Rabit core platform architecture

---
+++ data_room/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DataRoomConfig(AppConfig):
+    name = 'data_room'
---
+++ post_investment/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PostInvestmentConfig(AppConfig):
+    name = 'post_investment'
